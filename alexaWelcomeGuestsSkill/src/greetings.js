f6542ea125572620c4d9b9dd9bb1289f51ec36cb
Kamil Wolkowicz <kamil.wolkowicz@lppsa.com>
2018-01-14 18:08:13 +0100

    Fixed prepared greetings lines by adding SSML tags

--- alexaWelcomeGuestsSkill/src/greetings.js
+++ alexaWelcomeGuestsSkill/src/greetings.js
@@ -1,8 +1,8 @@
 module.exports = {
-"english class mates" : "Hi guys! Kamil told me that you wanted to play with me. I couldn't wait for this. I hope it will be great time!",
-"parents": "Hi Gosia nad Marek. I hope your trip was good.",
-"agata": "Hi Agata! How was your day? I hope it was good.",
-"michał and natalia": "Hi guys! How is your Alexa doing? I would like to finally meet her.",
-"andrzej": "Hi you fatty scum.",
-"my cats": "<speak><prosody pitch=\"x-high\">Captain Poora, <phoneme alphabet=\"ipa\" ph=\"Gra'ʒynə\">Grażyna! </phoneme> <break time=\"2s\"/> Come here! <break time=\"1s\"/> meow meow meow </prosody></speak>"
+"english class mates" : "<speak>Hi guys! Kamil told me that you wanted to play with me. I couldn't wait for this. I hope it will be great time!</speak>",
+"parents": "<speak>Hi Gosia nad Marek. I hope your trip was good.</speak>",
+"agata": "<speak>Hi Agata! How was your day? I hope it was good.</speak>",
+"michał and natalia": "<speak>Hi guys! How is your Alexa doing? I would like to finally meet her.</speak>",
+"andrzej": "<speak>Hi you fatty scum.</speak>",
+"my cats": "<speak><prosody pitch=\"x-high\">Captain Poora, <phoneme alphabet=\"ipa\" ph=\"Gra'ʒynə\">Grażyna! </phoneme> <break time=\"1s\"/> Come here! <break time=\"1s\"/> meow meow meow </prosody></speak>"
 };
